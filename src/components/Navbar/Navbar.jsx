2c998a2716c465d8a2ee1c3b275cfcbe4999ea1a
iinaamasum <iinaamasum@gmail.com>
2022-04-06 08:59:31 +0600

    navbar optimization

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BsMenuButtonWideFill } from 'react-icons/bs';
+import { GiCrossedBones } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
@@ -26,28 +28,45 @@ export default function Navbar() {
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-              click
+              {!navbarOpen ? <BsMenuButtonWideFill /> : <GiCrossedBones />}
             </button>
           </div>
           <div
             className={
-              'lg:flex flex-grow items-center' +
+              'lg:flex flex-grow items-between justify-end' +
               (navbarOpen ? ' flex' : ' hidden')
             }
             id="example-navbar-danger"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto items-center w-full md:w-auto">
-              {navLinks.map((link) => (
-                <li className="nav-item text-xl text-white md:ml-4">
-                  <Link
-                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                    to={link.path}
-                  >
-                    {link.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <div className="lg:flex lg:items-center lg:justify-center w-full lg:w-auto">
+              <ul className="flex flex-col lg:flex-row list-none lg:ml-auto items-left">
+                {navLinks.map((link) => (
+                  <li className="nav-item text-xl text-white md:ml-4">
+                    <Link
+                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      to={link.path}
+                    >
+                      {link.name}
+                    </Link>
+                  </li>
+                ))}
+                <div></div>
+              </ul>
+              <div className="flex items-center w-full lg:w-auto">
+                <button
+                  className="text-pink-500 border border-pink-500 hover:bg-pink-500 hover:text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 w-1/2 lg:w-auto"
+                  type="button"
+                >
+                  Regular
+                </button>
+                <button
+                  className="bg-sky-500 text-white active:bg-sky-600 font-bold uppercase text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 w-1/2 lg:w-auto"
+                  type="button"
+                >
+                  Regular
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
