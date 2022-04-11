2ecead9b270d38f461914d61daf4a67874443cd2
iinaamasum <iinaamasum@gmail.com>
2022-04-11 09:13:42 +0600

    login page design done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,10 +5,10 @@ import { FcGoogle } from 'react-icons/fc';
 const Login = () => {
   return (
     <>
-      <section>
-        <div className="w-1/2 mx-auto">
-          <div className="py-3">
-            <h3 className="tracking-wide text-center text-xl text-gray-300 pt-3 pb-2">
+      <section className="my-10">
+        <div className="w-2/5 mx-auto px-16 border-gray-700 shadow rounded-lg bg-gray-800 py-20">
+          <div className="pt-3 pb-10">
+            <h3 className="tracking-wide text-center text-md text-gray-300 py-4">
               Sign In with
             </h3>
             <div className="flex justify-center gap-3">
@@ -22,13 +22,62 @@ const Login = () => {
               </button>
             </div>
           </div>
-          <hr />
-          <div className="py-3">
-            <h3>Or sign in with credentials</h3>
-            <form>
-              <label htmlFor="email">Email</label>
-              <br />
-              <input type="email" name="" id="" placeholder="Your Email" />
+          <hr className="opacity-50" />
+          <div className="py-10">
+            <h3 className="tracking-wide text-center text-md text-gray-300 pt-3 pb-2">
+              Or sign in with credentials
+            </h3>
+            <form className="w-3/4 mx-auto">
+              <div className="">
+                <label
+                  className="text-xl text-slate-200 tracking-wide"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <br />
+                <input
+                  className="w-full rounded outline-none px-4 py-3 bg-slate-300 focus:bg-slate-100 text-black font-sans font-medium text-md"
+                  type="email"
+                  name=""
+                  id=""
+                  placeholder="Enter Your Email"
+                />
+              </div>
+              <div className="pt-4">
+                <label
+                  className="text-xl text-slate-200 tracking-wide"
+                  htmlFor="email"
+                >
+                  Password
+                </label>
+                <br />
+                <input
+                  className="w-full rounded outline-none px-4 py-3 bg-slate-300 focus:bg-slate-100 text-black font-sans font-medium text-md"
+                  type="password"
+                  name=""
+                  id=""
+                  placeholder="Enter Your Password"
+                />
+              </div>
+              <div className="pt-4 flex items-center justify-left">
+                <input
+                  className="w-4 text-cyan-500"
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <small className="pl-2 text-cyan-500 text-semibold tracking-wide">
+                  Remember Me
+                </small>
+              </div>
+              <div className="pt-8">
+                <input
+                  className="rounded tracking-wide text-slate-100 border-black bg-black hover:text-slate-400 w-full p-2 transition-all duration-300 ease-in-out text-xl cursor-pointer"
+                  type="submit"
+                  value="Sign In"
+                />
+              </div>
             </form>
           </div>
         </div>
